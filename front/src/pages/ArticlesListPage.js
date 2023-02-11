fab801cf7dd7acaf652a5275dbe5a7cd587a9cc2
jaydunb12 <74875595+jaydunb12@users.noreply.github.com>
2023-02-11 15:33:10 -0500

    update

--- front/src/pages/ArticlesListPage.js
+++ front/src/pages/ArticlesListPage.js
@@ -1,12 +1,57 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+
+
 
 const ArticlesListPage = () => {
   return (
-    <section className='about' id='about'>
-            <div className='title'>
-            <h2>Latest Notes</h2>
-            <p>Check out our latest notes by some of our members</p>
+    <section>
+      <h2 className='title'>Latest Post</h2>
+      <div className='contentBox'>
+        <div className='postColumn'>
+          <div className='postBox height'>
+            <div className='imgBox'>
+              <img src='https://images.pexels.com/photos/236111/pexels-photo-236111.jpeg' alt='pic'/>
+              <div className='textBx'>
+              <h2>Content Title</h2>
+
+              <h3>Just same text from one of our readers post. Signup to see more.....</h3>
+                <Link className='more' to="/" >Read More</Link>
+              </div>
+            </div>
+          </div>
+          <div className='postBox height'>
+            <div className='imgBox'>
+              <img src='https://images.pexels.com/photos/236111/pexels-photo-236111.jpeg' alt='pic'/>
+              <div className='textBx'>
+              <h2>Content Title</h2>
+              <h3>Just same text from one of our readers post. Signup to see more.....</h3>
+              <Link className='more' to="/" >Read More</Link>
+
+              </div>            </div>
+          </div>
+          <div className='postBox height'>
+            <div className='imgBox'>
+              <img src='https://images.pexels.com/photos/236111/pexels-photo-236111.jpeg' alt='pic'/>
+              <div className='textBx'>
+              <h2>Content Title</h2>
+              <h3>Just same text from one of our readers post. Signup to see more.....</h3>
+              <Link className='more' to="/" >Read More</Link>
+
+              </div>            </div>
+          </div>
+          <div className='postBox height'>
+            <div className='imgBox'>
+              <img src='https://images.pexels.com/photos/236111/pexels-photo-236111.jpeg' alt='pic'/>
+              <div className='textBx'>
+                <h2>Content Title</h2>
+              <h3>Just same text from one of our readers post. Signup to see more.....</h3>
+              <Link className='more' to="/" >Read More</Link>
+
+              </div>            </div>
+          </div>
         </div>
+      </div>
     </section>
   )
 }
